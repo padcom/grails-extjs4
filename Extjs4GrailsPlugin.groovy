9281ff0e21f4fdd8cb4cd4cee9f5d905b9d91843
Matthias Hryniszak <padcom@gmail.com>
2011-05-03 10:31:27 +0200

    Initial import

---
+++ Extjs4GrailsPlugin.groovy
@@ -0,0 +1,50 @@
+class Extjs4GrailsPlugin {
+    // the plugin version
+    def version = "4.0.0.0"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.2.0 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def author = "Matthias Hryniszak"
+    def authorEmail = "padcom@gmail.com"
+    def title = "Ext JS 4 library resources"
+    def description = '''\\
+This plugin provides resources needed to create an Ext JS 4 RIA"
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/extjs4"
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before 
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
---
+++ grails-app/views/layouts/ext.gsp
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title><g:layoutTitle default="Ext JS4"/></title>
+  <link rel="stylesheet" href="${resource(dir:'ext/resources/css', file: 'ext-all.css')}" />
+  <script type="text/javascript" src="${resource(dir:'ext', file: 'bootstrap.js')}"></script>
+  <g:layoutHead/>
+</head>
+
+<body>
+  <g:layoutBody/>
+</body>
+
+</html>
+  
